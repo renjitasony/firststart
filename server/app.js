eb36ec5f1e551a8222e24abb69db1a0737402964
renjitasony <renjitasony>
2019-08-26 22:40:39 +0530

    just done with view

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var mongoose =require('mongoose');
+
+var product = require('../rens/src/assets/model/product');
 
 const app = express();
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
-
+var url = "mongodb://localhost/mydb1";
+mongoose.connect(url,{useNewUrlParser:true},(err)=>{
+    if(err) throw err;
+    else{
+        console.log("connected");
+    }
+})
 app.use(function (req, res, next) {   
-
     res.setHeader('Access-Control-Allow-Origin', '*'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');  
@@ -20,4 +28,12 @@ app.listen(8000,(req,res)=>{
 });
 app.get("/data",(req,res)=>{
     res.send({msg:"data from server"})
-})
+});
+app.get("/viewdata",(req,res)=>{
+    product.findOne({},(err,result)=>{
+        if(err) throw err;
+        else{
+            res.send(result);    
+        }        
+    })
+});
